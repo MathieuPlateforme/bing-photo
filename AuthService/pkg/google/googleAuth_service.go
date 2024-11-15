6de192ada1c2b73c660dfc592330b19280d2223b
AlizeaMassePlat <alizea.masse@gmail.com>
2024-11-15 16:16:20 +0100

    add google service + google test and solidified database connection

--- AuthService/pkg/google/googleAuth_service.go
+++ AuthService/pkg/google/googleAuth_service.go
@@ -2,26 +2,65 @@ package google
 
 import (
 	"fmt"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"os"
+	"errors"
+	"encoding/json"
+	"net/http"
 )
 
 // GoogleAuthService structure
 type GoogleAuthService struct {
+	Config *oauth2.Config
 }
 
 // NewGoogleAuthService initialise et retourne une nouvelle instance de GoogleAuthService
 func NewGoogleAuthService() (*GoogleAuthService, error) {
 	fmt.Println("Initializing GoogleAuthService...")
-	return &GoogleAuthService{}, nil
+
+	// Initialiser la configuration OAuth2 pour Google
+	clientID := os.Getenv("GOOGLE_CLIENT_ID")
+	clientSecret := os.Getenv("GOOGLE_CLIENT_SECRET")
+	redirectURL := os.Getenv("GOOGLE_REDIRECT_URL")
+
+	if clientID == "" || clientSecret == "" || redirectURL == "" {
+		return nil, errors.New("Google OAuth2 configuration is missing")
+	}
+
+	config := &oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectURL,
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email","https://www.googleapis.com/auth/userinfo.profile"},
+		Endpoint:     google.Endpoint,
+	}
+
+	return &GoogleAuthService{Config: config}, nil
 }
 
-func (s *GoogleAuthService) AuthenticateWithGoogle() {
-	// Logique de connexion avec Google
+func (s *GoogleAuthService) AuthenticateWithGoogle() string {
+	// Génère et renvoie l'URL d'authentification Google
+	return s.Config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
 }
 
-func (s *GoogleAuthService) GetGoogleUserProfile() {
-	// Logique pour obtenir le profil de l'utilisateur Google
-}
+func (s *GoogleAuthService) GetGoogleUserProfile(token *oauth2.Token) (map[string]interface{}, error) {
+	client := s.Config.Client(oauth2.NoContext, token)
+	userInfoResp, err := client.Get("https://www.googleapis.com/oauth2/v3/userinfo")
+	if err != nil {
+		return nil, fmt.Errorf("Échec de la récupération des informations utilisateur : %v", err)
+	}
+	defer userInfoResp.Body.Close()
 
-func (s *GoogleAuthService) HandleAuthFailure() {
-// Logique pour gérer l'échec de l'authentification
+	// Vérifiez le code de statut HTTP de la réponse
+	if userInfoResp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Requête échouée avec le code de statut : %d", userInfoResp.StatusCode)
+	}
+
+	userInfo := map[string]interface{}{}
+	if err := json.NewDecoder(userInfoResp.Body).Decode(&userInfo); err != nil {
+		return nil, fmt.Errorf("Échec de la lecture des informations utilisateur : %v", err)
+	}
+
+	return userInfo, nil
 }
--- AuthService/services/handlers/auth_service_handlers.go
+++ AuthService/services/handlers/auth_service_handlers.go
@@ -3,15 +3,26 @@ package handlers
 import (
 	"net/http"
 	"AuthService/services/auth" 
+	"AuthService/pkg/google"
+	"golang.org/x/oauth2"
+	"fmt"
 )
 
 // Déclaration de AuthHandlers pour encapsuler AuthService
 type AuthHandlers struct {
 	Service *auth.AuthService
+	GoogleAuthService *google.GoogleAuthService
 }
 
-func NewAuthHandlers(service *auth.AuthService) *AuthHandlers {
-	return &AuthHandlers{Service: service}
+func NewAuthHandlers(service *auth.AuthService) (*AuthHandlers,error) {
+
+	// Initialiser le service GoogleAuthService
+	googleAuthService, err := google.NewGoogleAuthService()
+	if err != nil {
+		return nil, err
+	}
+
+	return &AuthHandlers{Service: service, GoogleAuthService: googleAuthService}, nil
 }
 
 func (h *AuthHandlers) LoginWithEmailHandler(w http.ResponseWriter, r *http.Request) {
@@ -35,8 +46,45 @@ func (h *AuthHandlers) ResetPasswordHandler(w http.ResponseWriter, r *http.Reque
 }
 
 func (h *AuthHandlers) LoginWithGoogleHandler(w http.ResponseWriter, r *http.Request) {
-	// Logique pour connecter un utilisateur avec Google
-	w.Write([]byte("LoginWithGoogle endpoint"))
+	// Appeler AuthenticateWithGoogle pour obtenir l'URL d'authentification
+	authURL := h.GoogleAuthService.AuthenticateWithGoogle()
+
+	// Rediriger l'utilisateur vers l'URL d'authentification Google
+	http.Redirect(w, r, authURL, http.StatusTemporaryRedirect)
+}
+
+func (h *AuthHandlers) GoogleAuthCallbackHandler(w http.ResponseWriter, r *http.Request) {
+	// Récupérer le code d'authentification de Google
+	code := r.URL.Query().Get("code")
+	if code == "" {
+		http.Error(w, "Code d'authentification manquant", http.StatusBadRequest)
+		return
+	}
+
+	// Récupérer l'état de l'authentification
+	state := r.URL.Query().Get("state")
+	if state != "state-token" {
+		http.Error(w, "État d'authentification invalide", http.StatusBadRequest)
+		return
+	}
+
+	// Échanger le code d'authentification contre un jeton d'accès
+	token, err := h.GoogleAuthService.Config.Exchange(oauth2.NoContext, code)
+	// log.Println("Token FOR TEST : ", token)
+	if err != nil {
+		http.Error(w, "Échec de l'échange de jeton : "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Obtenir le profil utilisateur en utilisant GoogleAuthService
+	userInfo, err := h.GoogleAuthService.GetGoogleUserProfile(token)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Renvoyer ou traiter les informations utilisateur 
+	fmt.Fprintf(w, "Informations utilisateur : %+v", userInfo)
 }
 
 func (h *AuthHandlers) ValidateTokenHandler(w http.ResponseWriter, r *http.Request) {
